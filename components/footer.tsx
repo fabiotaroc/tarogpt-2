38a391c70672175ee9bf432d02ac73a27086a32f
taro <fabiotaroc@gmail.com>
2024-10-19 16:37:45 +0200

    row data as type instead of any in results page

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,7 @@ export function FooterText({ className, ...props }: React.ComponentProps<'p'>) {
   return (
     <p
       className={cn(
-        'px-2 text-center text-xs leading-normal text-muted-foreground',
+        'px-2 text-center text-xs leading-normal text-secondary-foreground',
         className
       )}
       {...props}

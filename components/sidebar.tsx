e593aed465c69d6eceac301acf7dae3c8796f0e0
taro <fabiotaroc@gmail.com>
2024-10-20 19:10:37 +0200

    refactor(Sidebar): Remove empty interface and simplify props typing

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -5,9 +5,7 @@ import * as React from "react";
 import { useSidebar } from "@/lib/hooks/use-sidebar";
 import { cn } from "@/lib/utils";
 
-export interface SidebarProps extends React.ComponentProps<"div"> {}
-
-export function Sidebar({ className, children }: SidebarProps) {
+export function Sidebar({ className, children }: React.ComponentProps<"div">) {
   const { isSidebarOpen, isLoading } = useSidebar();
 
   return (
